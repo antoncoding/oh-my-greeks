4ac60d3192efb6e35fd41bba5d7278af1c1f5aca
Anton Cheng <antonassocareer@gmail.com>
2022-05-10 23:09:08 +0200

    misc: clean dataview

--- src/constants/dataviewContents.tsx
+++ src/constants/dataviewContents.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
-
-const emptyStateRedSrc = require('../imgs/aragon/empty-state-illustration-red.png')
-const EmptyState = <img src={emptyStateRedSrc} alt="" width="185" />
-
 export const POSITIONS = {
   default: {
-    title: 'No position',
-    subtitle: "You don't have any option position",
-    illustration: EmptyState,
+    title: 'No active positions',
+    subtitle: "You don't have exposure to any greeks on-chain",
+    illustration: null,
   },
   loading: {
     title: 'Loading',
     subtitle: 'Fetching user positions... ',
+    illustration: null,
+  },
+  'empty-filters': {
+    displayLoader: false,
+    title: 'No results found.',
+    subtitle: 'We can’t find any item matching your filter selection.',
+    illustration: null,
+    clearLabel: 'Clear filters',
   },
 }
